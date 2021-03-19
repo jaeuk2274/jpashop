87fe4cb5663d8f3fe29d95380a7e4c2639944b9d
jaeuk2274 <jaeuk2274@gmail.com>
2021-03-19 16:34:55 +0900

    Order domain

--- src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -25,4 +25,18 @@ public class OrderItem {
 
     private int orderPrice;
     private int count;
+
+    //==비즈니스 로직==//
+    public void cancel() {
+        getItem().addStock(count);
+    }
+
+    //==조회 로직==//
+
+    /**
+     * 주문상품 전체 가격 조회
+     */
+    public int getTotalPrice() {
+        return getOrderPrice() * getCount();
+    }
 }
